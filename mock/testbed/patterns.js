6f44e05cb8df4e4e3816726beabc8d25d65a31af
ampcpmgp <ampcpmgp@gmail.com>
2021-04-10 23:00:40 +0900

    first commit

---
+++ mock/testbed/patterns.js
@@ -0,0 +1,5 @@
+import { generateList } from "am-mocktimes/lib/main";
+import pattern from "../patterns.yml";
+
+console.log(generateList);
+generateList(pattern);
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig, resolveConfig } from "vite";
+import svelte from "@sveltejs/vite-plugin-svelte";
+import yaml from "@rollup/plugin-yaml";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), yaml()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        patterns: resolve(__dirname, "mock/testbed/patterns.html"),
+        // mock: resolve(__dirname, "mock/testbed/mock.html"),
+      },
+    },
+  },
+});
