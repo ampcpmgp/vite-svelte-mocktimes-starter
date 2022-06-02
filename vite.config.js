0b6da21a24de9be4ebf5cd900bf353d85e5bc3e2
ampcpmgp <ampcpmgp@gmail.com>
2022-06-02 21:24:33 +0900

    version update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import svelte from "@sveltejs/vite-plugin-svelte";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import yaml from "@rollup/plugin-yaml";
 import { resolve } from "path";
 
